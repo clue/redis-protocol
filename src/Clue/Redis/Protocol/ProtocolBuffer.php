072b4218d6963e8f704a3b1d4a48cbc2b27c3287
Christian Lück <christian@lueck.tv>
2013-07-09 14:30:06 +0200

    Implement parser from jdp/redisent

--- src/Clue/Redis/Protocol/ProtocolBuffer.php
+++ src/Clue/Redis/Protocol/ProtocolBuffer.php
@@ -4,6 +4,7 @@ namespace Clue\Redis\Protocol;
 
 use Clue\Redis\Protocol\ProtocolInterface;
 use UnderflowException;
+use Exception;
 
 /**
  * Simple redis wire protocol parser and serializer
@@ -18,6 +19,7 @@ class ProtocolBuffer implements ProtocolInterface
     const CRLF = "\r\n";
 
     private $incomingBuffer = '';
+    private $incomingOffset = 0;
     private $incomingQueue = array();
 
     public function pushIncoming($dataChunk)
@@ -50,6 +52,116 @@ class ProtocolBuffer implements ProtocolInterface
 
     private function tryParsingIncomingMessages()
     {
+        do {
+            try {
+                $message = $this->readResponse();
+            }
+            catch (Exception $e) {
+                // restore previous position for next parsing attempt
+                $this->incomingOffset = 0;
+                break;
+            }
 
+            $this->incomingQueue []= $message;
+
+            $this->incomingBuffer = (string)substr($this->incomingBuffer, $this->incomingOffset);
+            $this->incomingOffset = 0;
+        } while($this->incomingBuffer !== '');
+    }
+
+    private function readLine()
+    {
+        $pos = strpos($this->incomingBuffer, "\r\n", $this->incomingOffset);
+
+        if ($pos === false) {
+            throw new Exception('Unable to find CRLF sequence');
+        }
+
+        $ret = (string)substr($this->incomingBuffer, $this->incomingOffset, $pos - $this->incomingOffset);
+        $this->incomingOffset = $pos + 2;
+
+        return $ret;
+    }
+
+    private function readLength($len)
+    {
+        $ret = substr($this->incomingBuffer, $this->incomingOffset, $len);
+        if (strlen($ret) !== $len) {
+            throw new Exception('Unable to read requested number of bytes');
+        }
+
+        $this->incomingOffset += $len;
+
+        return $ret;
+    }
+
+    /**
+     * try to parse response from incoming buffer
+     *
+     * ripped from jdp/redisent, with some minor modifications to read from
+     * the incoming buffer instead of issuing a blocking fread on a stream
+     *
+     * @throws Exception
+     * @return mixed
+     * @link https://github.com/jdp/redisent
+     */
+    private function readResponse() {
+        /* Parse the response based on the reply identifier */
+        $reply = trim($this->readLine());
+        switch (substr($reply, 0, 1)) {
+            /* Error reply */
+            case '-':
+                throw new Exception(trim(substr($reply, 4)));
+                break;
+                /* Inline reply */
+            case '+':
+                $response = substr(trim($reply), 1);
+                if ($response === 'OK') {
+                    $response = TRUE;
+                }
+                break;
+                /* Bulk reply */
+            case '$':
+                $response = NULL;
+                if ($reply == '$-1') {
+                    break;
+                }
+                $read = 0;
+                $size = intval(substr($reply, 1));
+                if ($size > 0) {
+                    do {
+                        $block_size = ($size - $read) > 1024 ? 1024 : ($size - $read);
+                        $r = $this->readLength($block_size);
+                        if ($r === FALSE) {
+                            throw new Exception('Failed to read response from stream');
+                        } else {
+                            $read += strlen($r);
+                            $response .= $r;
+                        }
+                    } while ($read < $size);
+                }
+                $this->readLength(2); /* discard crlf */
+                break;
+                /* Multi-bulk reply */
+            case '*':
+                $count = intval(substr($reply, 1));
+                if ($count == '-1') {
+                    return NULL;
+                }
+                $response = array();
+                for ($i = 0; $i < $count; $i++) {
+                    $response[] = $this->readResponse();
+                }
+                break;
+                /* Integer reply */
+            case ':':
+                $response = intval(substr(trim($reply), 1));
+                break;
+            default:
+                throw new Exception("Unknown response: {$reply}");
+                break;
+        }
+        /* Party on */
+        return $response;
     }
 }
