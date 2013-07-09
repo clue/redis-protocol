d915a29c086c3ce982ffb000391a9a01eb9f6a73
Christian Lück <christian@lueck.tv>
2013-07-09 12:05:43 +0200

    Skeleton for parsing and queueing incoming messages

--- src/Clue/Redis/Protocol/ProtocolBuffer.php
+++ src/Clue/Redis/Protocol/ProtocolBuffer.php
@@ -3,6 +3,7 @@
 namespace Clue\Redis\Protocol;
 
 use Clue\Redis\Protocol\ProtocolInterface;
+use UnderflowException;
 
 /**
  * Simple redis wire protocol parser and serializer
@@ -17,20 +18,27 @@ class ProtocolBuffer implements ProtocolInterface
     const CRLF = "\r\n";
 
     private $incomingBuffer = '';
+    private $incomingQueue = array();
 
     public function pushIncoming($dataChunk)
     {
         $this->incomingBuffer .= $dataChunk;
+
+        $this->tryParsingIncomingMessages();
     }
 
     public function popIncoming()
     {
-
+        $message = array_shift($this->incomingQueue);
+        if ($message === null) {
+            throw new UnderflowException('Incoming message queue is empty');
+        }
+        return $message;
     }
 
     public function hasIncoming()
     {
-
+        return ($this->incomingQueue) ? true : false;
     }
 
     public function createMessage(array $args)
@@ -38,5 +46,10 @@ class ProtocolBuffer implements ProtocolInterface
     	return sprintf('*%d%s%s%s', count($args), ProtocolBuffer::CRLF, implode(array_map(function($arg) {
             return sprintf('$%d%s%s', strlen($arg), ProtocolBuffer::CRLF, $arg);
         }, $args), ProtocolBuffer::CRLF), ProtocolBuffer::CRLF);
+    }
+
+    private function tryParsingIncomingMessages()
+    {
+
     }
 }
