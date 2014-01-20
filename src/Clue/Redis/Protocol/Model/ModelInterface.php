9823baede8a2e00fa84cecda1b47ccfbd587bcbf
Christian Lück <christian@lueck.tv>
2014-01-20 13:51:20 +0100

    Improve documentation

--- src/Clue/Redis/Protocol/Model/ModelInterface.php
+++ src/Clue/Redis/Protocol/Model/ModelInterface.php
@@ -6,7 +6,17 @@ interface ModelInterface
 {
     const CRLF = "\r\n";
 
+    /**
+     * Returns value of this model as a native representation for PHP
+     *
+     * @return mixed
+     */
     public function getValueNative();
 
+    /**
+     * Returns the serialized representation of this protocol message
+     *
+     * @return string
+     */
     public function getMessageSerialized();
 }
